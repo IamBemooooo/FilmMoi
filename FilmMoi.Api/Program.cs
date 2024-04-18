3ddcf6ed6a23a333d011977242855e6619856ae0
SuRinRin <dangnguyen300708@gmail.com>
2024-04-18 20:10:19 +0700

    aaa

--- FilmMoi.Api/Program.cs
+++ FilmMoi.Api/Program.cs
@@ -1,4 +1,11 @@
+using AutoMapper;
+using FilmMoi.Application.Interface.ReadOnly;
+using FilmMoi.Application.Interface.ReadWrite;
 using FilmMoi.Application.ValueObj.Extentions;
+using FilmMoi.Domain.Models;
+using FilmMoi.Domain.Models.Entities;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddApplication();
 // Add services to the container.
