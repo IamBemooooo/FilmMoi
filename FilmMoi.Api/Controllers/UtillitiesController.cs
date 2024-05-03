9829e02f857f16ccf7c025a05c2c9a82e82a9c49
SuRinRin <dangnguyen300708@gmail.com>
2024-05-03 22:07:58 +0700

    3/5

--- FilmMoi.Api/Controllers/UtillitiesController.cs
+++ FilmMoi.Api/Controllers/UtillitiesController.cs
@@ -19,7 +19,7 @@ namespace FilmMoi.Api.Controllers
         [HttpPost]
         public async Task<IActionResult> Post([FromBody]ForgotPasswordRequest request,CancellationToken cancellationToken)
         {
-            var result = await _repo.ForgotPassword(request, cancellationToken);
+            var result = await _repo.SeedGmail(request, cancellationToken);
             return Ok(result);
         }
     }
